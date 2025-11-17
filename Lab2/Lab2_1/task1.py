4490e6d3bd3b9a7fbc924544981de7f37c9a082b
DariaSeniutych <dariaseniutych@yandex.by>
2025-11-17 09:28:18 +0300

    исправлен баг, где слова по 2 не считались

--- Lab2/Lab2_1/task1.py
+++ Lab2/Lab2_1/task1.py
@@ -1,12 +1,13 @@
 text = input("введи текст: ")
 words = text.split()
 word_slovar = {}
+
 for word in words:
     word = word.lower()
-if word in word_slovar:
-    word_slovar[word] = word_slovar[word] + 1
-else:
-    word_slovar[word] = 1
+    if word in word_slovar:
+        word_slovar[word] = word_slovar[word] + 1
+    else:
+        word_slovar[word] = 1
 
 print("сколько раз каждое слово встречается:")
 print(word_slovar)
